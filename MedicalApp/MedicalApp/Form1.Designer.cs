594130b2107298d0c6c514200e8ba800162d9a9d
kev-69 <bismarkobuobi19@gmail.com>
2025-08-22 18:51:04 +0000

    Rename Form1 to MainForm and update UI components with event handlers for navigation and functionality

--- MedicalApp/MedicalApp/Form1.Designer.cs
+++ MedicalApp/MedicalApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MedicalApp
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnDoctorList = new System.Windows.Forms.Button();
+            this.btnBookAppointment = new System.Windows.Forms.Button();
+            this.btnManageAppointments = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(200, 50);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(400, 26);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Medical Appointment Booking System";
+            // 
+            // btnDoctorList
+            // 
+            this.btnDoctorList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoctorList.Location = new System.Drawing.Point(300, 120);
+            this.btnDoctorList.Name = "btnDoctorList";
+            this.btnDoctorList.Size = new System.Drawing.Size(200, 50);
+            this.btnDoctorList.TabIndex = 1;
+            this.btnDoctorList.Text = "View Doctors";
+            this.btnDoctorList.UseVisualStyleBackColor = true;
+            this.btnDoctorList.Click += new System.EventHandler(this.btnDoctorList_Click);
+            // 
+            // btnBookAppointment
+            // 
+            this.btnBookAppointment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBookAppointment.Location = new System.Drawing.Point(300, 190);
+            this.btnBookAppointment.Name = "btnBookAppointment";
+            this.btnBookAppointment.Size = new System.Drawing.Size(200, 50);
+            this.btnBookAppointment.TabIndex = 2;
+            this.btnBookAppointment.Text = "Book Appointment";
+            this.btnBookAppointment.UseVisualStyleBackColor = true;
+            this.btnBookAppointment.Click += new System.EventHandler(this.btnBookAppointment_Click);
+            // 
+            // btnManageAppointments
+            // 
+            this.btnManageAppointments.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManageAppointments.Location = new System.Drawing.Point(300, 260);
+            this.btnManageAppointments.Name = "btnManageAppointments";
+            this.btnManageAppointments.Size = new System.Drawing.Size(200, 50);
+            this.btnManageAppointments.TabIndex = 3;
+            this.btnManageAppointments.Text = "Manage Appointments";
+            this.btnManageAppointments.UseVisualStyleBackColor = true;
+            this.btnManageAppointments.Click += new System.EventHandler(this.btnManageAppointments_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(300, 330);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(200, 50);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnManageAppointments);
+            this.Controls.Add(this.btnBookAppointment);
+            this.Controls.Add(this.btnDoctorList);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Medical Appointment System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnDoctorList;
+        private System.Windows.Forms.Button btnBookAppointment;
+        private System.Windows.Forms.Button btnManageAppointments;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
