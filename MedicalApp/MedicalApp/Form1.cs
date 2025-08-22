59aabb3d835981ecf2d90e3ffc10d562c7254cd3
kev-69 <bismarkobuobi19@gmail.com>
2025-08-22 18:50:55 +0000

    Rename Form1 to MainForm and implement button click event handlers for navigating to DoctorList, Appointment, and ManageAppointments forms

--- MedicalApp/MedicalApp/Form1.cs
+++ MedicalApp/MedicalApp/Form1.cs
@@ -10,11 +10,34 @@ using System.Windows.Forms;
 
 namespace MedicalApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnDoctorList_Click(object sender, EventArgs e)
+        {
+            DoctorListForm doctorForm = new DoctorListForm();
+            doctorForm.Show();
+        }
+
+        private void btnBookAppointment_Click(object sender, EventArgs e)
+        {
+            AppointmentForm appointmentForm = new AppointmentForm();
+            appointmentForm.Show();
+        }
+
+        private void btnManageAppointments_Click(object sender, EventArgs e)
+        {
+            ManageAppointmentsForm manageForm = new ManageAppointmentsForm();
+            manageForm.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
