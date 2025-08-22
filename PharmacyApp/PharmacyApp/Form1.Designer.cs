d4e6d858d4fa03315375c614d5b01b794cb26405
kev-69 <bismarkobuobi19@gmail.com>
2025-08-22 18:52:47 +0000

    Rename Form1 to PharmacyMainForm and enhance UI components for better functionality

--- PharmacyApp/PharmacyApp/Form1.Designer.cs
+++ PharmacyApp/PharmacyApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PharmacyApp
 {
-    partial class Form1
+    partial class PharmacyMainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,309 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.groupBoxAddMedicine = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAddMedicine = new System.Windows.Forms.Button();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.txtMedicineName = new System.Windows.Forms.TextBox();
+            this.lblMedicineName = new System.Windows.Forms.Label();
+            this.groupBoxSearch = new System.Windows.Forms.GroupBox();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearchTerm = new System.Windows.Forms.TextBox();
+            this.lblSearchTerm = new System.Windows.Forms.Label();
+            this.dgvMedicines = new System.Windows.Forms.DataGridView();
+            this.groupBoxActions = new System.Windows.Forms.GroupBox();
+            this.btnRecordSale = new System.Windows.Forms.Button();
+            this.btnUpdateStock = new System.Windows.Forms.Button();
+            this.groupBoxAddMedicine.SuspendLayout();
+            this.groupBoxSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicines)).BeginInit();
+            this.groupBoxActions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(400, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(300, 26);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Pharmacy Management System";
+            // 
+            // groupBoxAddMedicine
+            // 
+            this.groupBoxAddMedicine.Controls.Add(this.btnClear);
+            this.groupBoxAddMedicine.Controls.Add(this.btnAddMedicine);
+            this.groupBoxAddMedicine.Controls.Add(this.txtQuantity);
+            this.groupBoxAddMedicine.Controls.Add(this.lblQuantity);
+            this.groupBoxAddMedicine.Controls.Add(this.txtPrice);
+            this.groupBoxAddMedicine.Controls.Add(this.lblPrice);
+            this.groupBoxAddMedicine.Controls.Add(this.txtCategory);
+            this.groupBoxAddMedicine.Controls.Add(this.lblCategory);
+            this.groupBoxAddMedicine.Controls.Add(this.txtMedicineName);
+            this.groupBoxAddMedicine.Controls.Add(this.lblMedicineName);
+            this.groupBoxAddMedicine.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxAddMedicine.Location = new System.Drawing.Point(30, 70);
+            this.groupBoxAddMedicine.Name = "groupBoxAddMedicine";
+            this.groupBoxAddMedicine.Size = new System.Drawing.Size(350, 250);
+            this.groupBoxAddMedicine.TabIndex = 1;
+            this.groupBoxAddMedicine.TabStop = false;
+            this.groupBoxAddMedicine.Text = "Add New Medicine";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(180, 200);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 30);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAddMedicine
+            // 
+            this.btnAddMedicine.BackColor = System.Drawing.Color.Green;
+            this.btnAddMedicine.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMedicine.ForeColor = System.Drawing.Color.White;
+            this.btnAddMedicine.Location = new System.Drawing.Point(270, 200);
+            this.btnAddMedicine.Name = "btnAddMedicine";
+            this.btnAddMedicine.Size = new System.Drawing.Size(75, 30);
+            this.btnAddMedicine.TabIndex = 8;
+            this.btnAddMedicine.Text = "Add";
+            this.btnAddMedicine.UseVisualStyleBackColor = false;
+            this.btnAddMedicine.Click += new System.EventHandler(this.btnAddMedicine_Click);
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(120, 150);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(200, 21);
+            this.txtQuantity.TabIndex = 7;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(20, 153);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(55, 15);
+            this.lblQuantity.TabIndex = 6;
+            this.lblQuantity.Text = "Quantity:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(120, 110);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(200, 21);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(20, 113);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(38, 15);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "Price:";
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategory.Location = new System.Drawing.Point(120, 70);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(200, 21);
+            this.txtCategory.TabIndex = 3;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategory.Location = new System.Drawing.Point(20, 73);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(58, 15);
+            this.lblCategory.TabIndex = 2;
+            this.lblCategory.Text = "Category:";
+            // 
+            // txtMedicineName
+            // 
+            this.txtMedicineName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMedicineName.Location = new System.Drawing.Point(120, 30);
+            this.txtMedicineName.Name = "txtMedicineName";
+            this.txtMedicineName.Size = new System.Drawing.Size(200, 21);
+            this.txtMedicineName.TabIndex = 1;
+            // 
+            // lblMedicineName
+            // 
+            this.lblMedicineName.AutoSize = true;
+            this.lblMedicineName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedicineName.Location = new System.Drawing.Point(20, 33);
+            this.lblMedicineName.Name = "lblMedicineName";
+            this.lblMedicineName.Size = new System.Drawing.Size(94, 15);
+            this.lblMedicineName.TabIndex = 0;
+            this.lblMedicineName.Text = "Medicine Name:";
+            // 
+            // groupBoxSearch
+            // 
+            this.groupBoxSearch.Controls.Add(this.btnViewAll);
+            this.groupBoxSearch.Controls.Add(this.btnSearch);
+            this.groupBoxSearch.Controls.Add(this.txtSearchTerm);
+            this.groupBoxSearch.Controls.Add(this.lblSearchTerm);
+            this.groupBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxSearch.Location = new System.Drawing.Point(400, 70);
+            this.groupBoxSearch.Name = "groupBoxSearch";
+            this.groupBoxSearch.Size = new System.Drawing.Size(350, 100);
+            this.groupBoxSearch.TabIndex = 2;
+            this.groupBoxSearch.TabStop = false;
+            this.groupBoxSearch.Text = "Search Medicines";
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewAll.Location = new System.Drawing.Point(260, 50);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(75, 25);
+            this.btnViewAll.TabIndex = 3;
+            this.btnViewAll.Text = "View All";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            this.btnViewAll.Click += new System.EventHandler(this.btnViewAll_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(180, 50);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 25);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearchTerm
+            // 
+            this.txtSearchTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchTerm.Location = new System.Drawing.Point(100, 23);
+            this.txtSearchTerm.Name = "txtSearchTerm";
+            this.txtSearchTerm.Size = new System.Drawing.Size(235, 21);
+            this.txtSearchTerm.TabIndex = 1;
+            // 
+            // lblSearchTerm
+            // 
+            this.lblSearchTerm.AutoSize = true;
+            this.lblSearchTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearchTerm.Location = new System.Drawing.Point(20, 26);
+            this.lblSearchTerm.Name = "lblSearchTerm";
+            this.lblSearchTerm.Size = new System.Drawing.Size(75, 15);
+            this.lblSearchTerm.TabIndex = 0;
+            this.lblSearchTerm.Text = "Search Term:";
+            // 
+            // dgvMedicines
+            // 
+            this.dgvMedicines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMedicines.Location = new System.Drawing.Point(30, 340);
+            this.dgvMedicines.Name = "dgvMedicines";
+            this.dgvMedicines.Size = new System.Drawing.Size(720, 300);
+            this.dgvMedicines.TabIndex = 3;
+            this.dgvMedicines.SelectionChanged += new System.EventHandler(this.dgvMedicines_SelectionChanged);
+            // 
+            // groupBoxActions
+            // 
+            this.groupBoxActions.Controls.Add(this.btnRecordSale);
+            this.groupBoxActions.Controls.Add(this.btnUpdateStock);
+            this.groupBoxActions.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxActions.Location = new System.Drawing.Point(400, 190);
+            this.groupBoxActions.Name = "groupBoxActions";
+            this.groupBoxActions.Size = new System.Drawing.Size(350, 130);
+            this.groupBoxActions.TabIndex = 4;
+            this.groupBoxActions.TabStop = false;
+            this.groupBoxActions.Text = "Medicine Actions";
+            // 
+            // btnRecordSale
+            // 
+            this.btnRecordSale.BackColor = System.Drawing.Color.Blue;
+            this.btnRecordSale.Enabled = false;
+            this.btnRecordSale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecordSale.ForeColor = System.Drawing.Color.White;
+            this.btnRecordSale.Location = new System.Drawing.Point(180, 40);
+            this.btnRecordSale.Name = "btnRecordSale";
+            this.btnRecordSale.Size = new System.Drawing.Size(120, 50);
+            this.btnRecordSale.TabIndex = 1;
+            this.btnRecordSale.Text = "Record Sale";
+            this.btnRecordSale.UseVisualStyleBackColor = false;
+            this.btnRecordSale.Click += new System.EventHandler(this.btnRecordSale_Click);
+            // 
+            // btnUpdateStock
+            // 
+            this.btnUpdateStock.BackColor = System.Drawing.Color.Orange;
+            this.btnUpdateStock.Enabled = false;
+            this.btnUpdateStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateStock.ForeColor = System.Drawing.Color.White;
+            this.btnUpdateStock.Location = new System.Drawing.Point(40, 40);
+            this.btnUpdateStock.Name = "btnUpdateStock";
+            this.btnUpdateStock.Size = new System.Drawing.Size(120, 50);
+            this.btnUpdateStock.TabIndex = 0;
+            this.btnUpdateStock.Text = "Update Stock";
+            this.btnUpdateStock.UseVisualStyleBackColor = false;
+            this.btnUpdateStock.Click += new System.EventHandler(this.btnUpdateStock_Click);
+            // 
+            // PharmacyMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(780, 660);
+            this.Controls.Add(this.groupBoxActions);
+            this.Controls.Add(this.dgvMedicines);
+            this.Controls.Add(this.groupBoxSearch);
+            this.Controls.Add(this.groupBoxAddMedicine);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "PharmacyMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pharmacy Management System";
+            this.groupBoxAddMedicine.ResumeLayout(false);
+            this.groupBoxAddMedicine.PerformLayout();
+            this.groupBoxSearch.ResumeLayout(false);
+            this.groupBoxSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicines)).EndInit();
+            this.groupBoxActions.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.GroupBox groupBoxAddMedicine;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAddMedicine;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.TextBox txtMedicineName;
+        private System.Windows.Forms.Label lblMedicineName;
+        private System.Windows.Forms.GroupBox groupBoxSearch;
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearchTerm;
+        private System.Windows.Forms.Label lblSearchTerm;
+        private System.Windows.Forms.DataGridView dgvMedicines;
+        private System.Windows.Forms.GroupBox groupBoxActions;
+        private System.Windows.Forms.Button btnRecordSale;
+        private System.Windows.Forms.Button btnUpdateStock;
     }
 }
 
